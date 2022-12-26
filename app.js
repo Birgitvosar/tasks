a5ba1fea40f664091a189e8dff8c0b2d33dcd6c6
Birgit Võsar <birgit.vosar@gmail.com>
2022-12-26 17:03:38 +0200

    add task to tasks list

---
+++ app.js
@@ -0,0 +1,23 @@
+const form = document.querySelector( 'form');
+const taskInput = document.querySelector('#task');
+
+form.addEventListener('submit', addTask)
+
+function addTask(e) {
+    // input value
+    const task = taskInput.value;
+    // create <li> element
+    const li = document.createElement('li');
+    //define <li> CSS class
+    li.className = "collection-item";
+    // create text value to <li>
+    li.appendChild(text);
+    // find <ul> DOM component
+    const ul = document.querySelector('.collection');
+    //add <li> to <ul>
+    ul.appendChild(li);
+    //clear input value
+    taskInput.value = '';
+    //form submit event control
+    e.preventDefault();
+}
